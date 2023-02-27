fb27177c7b9e1e92b19fa9aee9a8311a5205aa19
Yaroslav <yaroslav.kramarchuk@gmail.com>
2023-02-27 18:31:41 +0100

    code splitting and styles added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from 'components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/https://github.com/YarKram/goit-react-hw-05-movies-01">
     <React.StrictMode>
       <App />
     </React.StrictMode>
